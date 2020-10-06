3ad79b20fa4b396b5488c128aead3a8d63d702f8
dragon <vm>
2020-10-06 23:12:46 +0800

    n

--- src/demo_keyword_defer.go
+++ src/demo_keyword_defer.go
@@ -2,9 +2,11 @@ package main
 
 //https://draveness.me/golang/docs/part2-foundation/ch05-keyword/golang-defer/
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main2() {
+func main_defer2() {
 	{
 		defer fmt.Println("defer runs")
 		fmt.Println("block ends")
@@ -13,13 +15,14 @@ func main2() {
 	fmt.Println("main ends")
 }
 
-func main1() {
+func main_defer1() {
 	for i := 0; i < 5; i++ {
 		defer fmt.Println(i)
 	}
 }
 
 func main() {
-	main1()
-	main2()
+	//main1()
+	main_defer1()
+	main_defer2()
 }
