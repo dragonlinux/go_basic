56f191779ca02dc650cb66478e0caf7fe6660514
dragon <vm>
2020-10-11 21:33:41 +0800

    n

--- src/mqttPublish/demo_mqtt_publish.go
+++ src/mqttPublish/demo_mqtt_publish.go
@@ -7,9 +7,37 @@ import (
 	"time"
 )
 
+func createMqttClient(clientID string, uri *url.URL) (mqtt.Client, error) {
+	fmt.Println(fmt.Sprintf("Create MQTT client and connection: uri=%v clientID=%v ", uri.String(), clientID))
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("%s://%s", uri.Scheme, uri.Host))
+	opts.SetClientID(clientID)
+	opts.SetUsername(uri.User.Username())
+	password, _ := uri.User.Password()
+	opts.SetPassword(password)
+
+	//opts.SetConnectionLostHandler(func(client mqtt.Client, e error) {
+	//	fmt.Println(fmt.Sprintf("Connection lost : %v", e))
+	//	token := client.Connect()
+	//	if token.Wait() && token.Error() != nil {
+	//		fmt.Println(fmt.Sprintf("Reconnection failed : %v", e))
+	//	} else {
+	//		fmt.Println(fmt.Sprintf("Reconnection sucessful : %v", e))
+	//	}
+	//})
+
+	client := mqtt.NewClient(opts)
+	token := client.Connect()
+	if token.Wait() && token.Error() != nil {
+		return client, token.Error()
+	}
+
+	return client, nil
+}
+
 func mqttPublish(content string) {
 
-	var brokerUrl = "192.168.1.190"
+	var brokerUrl = "192.168.1.80"
 	var brokerPort = 1883
 	var username = "admin"
 	var password = "public"
@@ -41,7 +69,7 @@ func mqttPublish(content string) {
 func forLoop(i int) {
 	for {
 		mqttPublish(fmt.Sprintf("%v", i))
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(1000 * time.Millisecond)
 	}
 }
 
@@ -49,14 +77,7 @@ func forLoop(i int) {
 func operator() {
 	for i := 0; i < 5; i++ {
 		go forLoop(i)
-		//select {}
 	}
-
-	//go forLoop(1)
-	//go forLoop(2)
-	//go forLoop(3)
-	//go forLoop(4)
-	//go forLoop(5)
 
 	select {}
 }
@@ -65,32 +86,4 @@ func main() {
 	//mosquitto_pub -h 192.168.1.190 -t "DataTopic" -m "Hello MQTT1"
 	//mosquitto_sub -h 192.168.1.190 -t "DataTopic" -v
 	operator()
-}
-
-func createMqttClient(clientID string, uri *url.URL) (mqtt.Client, error) {
-	fmt.Println(fmt.Sprintf("Create MQTT client and connection: uri=%v clientID=%v ", uri.String(), clientID))
-	opts := mqtt.NewClientOptions()
-	opts.AddBroker(fmt.Sprintf("%s://%s", uri.Scheme, uri.Host))
-	opts.SetClientID(clientID)
-	opts.SetUsername(uri.User.Username())
-	password, _ := uri.User.Password()
-	opts.SetPassword(password)
-
-	//opts.SetConnectionLostHandler(func(client mqtt.Client, e error) {
-	//	fmt.Println(fmt.Sprintf("Connection lost : %v", e))
-	//	token := client.Connect()
-	//	if token.Wait() && token.Error() != nil {
-	//		fmt.Println(fmt.Sprintf("Reconnection failed : %v", e))
-	//	} else {
-	//		fmt.Println(fmt.Sprintf("Reconnection sucessful : %v", e))
-	//	}
-	//})
-
-	client := mqtt.NewClient(opts)
-	token := client.Connect()
-	if token.Wait() && token.Error() != nil {
-		return client, token.Error()
-	}
-
-	return client, nil
 }
