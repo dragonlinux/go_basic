f9f45e2f4f3d975fa83e16ea64b04318f47d416d
dragon <vm>
2020-10-12 11:51:24 +0800

    n

--- src/EdgeX_ThingsBoard_c2d_v1.go
+++ src/EdgeX_ThingsBoard_c2d_v1.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -16,4 +17,20 @@ func main() {
 
 	log.Println("yamlFile:", yamlFile)
 
+	{
+		m := map[string]interface{}{}
+		// Parsing/Unmarshalling JSON encoding/json
+		err = json.Unmarshal([]byte(yamlFile), &m)
+		if err != nil {
+			panic(err)
+		}
+
+		parseMap(m)
+	}
+}
+
+func parseMap(aMap map[string]interface{}) {
+	for key, value := range aMap {
+		fmt.Println(key, ":", value)
+	}
 }
