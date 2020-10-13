c2bdbb8bf615a7e316137392953ecb0327f87fd0
dragon <vm>
2020-10-13 09:40:34 +0800

    n

--- src/basic_function.go
+++ src/basic_function.go
@@ -6,9 +6,21 @@ func sum(x int, y int) int {
 	return x + y
 }
 
-func main() {
-
+func testSum() {
 	result := sum(500, 10000)
-
 	fmt.Println(result)
+}
+
+func functionReturnMultiValue(x int, y int) (aInt int, bInt int, cBool bool) {
+	return 3, 7, false
+}
+
+func testFunctionReturnMultiValue() {
+	a, b, c := functionReturnMultiValue(0, 0)
+	fmt.Println(a, b, c)
+}
+
+func main() {
+	testSum()
+	testFunctionReturnMultiValue()
 }
